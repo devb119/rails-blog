7ac1d91800d566923680eddd682335ed607c21ed
devb119 <ducanh11092001@gmail.com>
2023-01-20 18:01:52 +0000

    add rich text editor

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,27 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
+
+document.addEventListener('turbolinks:load', () => {
+  document.addEventListener('click', () => {
+    let element = event.target.closest('.paragraph-content')
+    if (!element) return;
+
+    element.classList.add('d-none')
+    element.nextElementSibling.classList.remove('d-none')
+  })
+
+  document.addEventListener('click', () => {
+    if (!event.target.matches('.cancel')) return;
+
+    event.preventDefault();
+
+    let element = event.target.closest('.paragraph-form')
+
+    element.classList.add('d-none')
+    element.previousElementSibling.classList.remove('d-none')
+  })
+})
--- app/models/element.rb
+++ app/models/element.rb
@@ -2,4 +2,6 @@ class Element < ApplicationRecord
   belongs_to :post
   
   validates :element_type, inclusion: { in: ['paragraph', 'image', 'video-embed'] }
+  
+  has_rich_text :content
 end
