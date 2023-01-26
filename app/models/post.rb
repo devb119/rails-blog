bb3b82282fdfcc2ee4dc91ce5f8c45f52355061e
chung <phamthanhchung2001@gmail.com>
2023-01-26 16:45:19 +0000

    add validate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,17 @@
 class Post < ApplicationRecord
+    extend FriendlyId
+  friendly_id :title, use: [:slugged, :history, :finders]
+    
   belongs_to :author
   has_many :elements
   
   has_one_attached :header_image
+  
+  validates_presence_of :title, :description
+  validates_length_of :description, within: 50..250
+  
+  def should_generate_new_friendly_id?
+      title_changed?
+     
+  end
 end
